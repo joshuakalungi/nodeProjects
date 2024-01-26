0f28b9f2957cc543b820a43dce02955f948f5aaf
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-26 12:50:21 +0300

    sample node.js app

---
+++ hello-world.js
@@ -0,0 +1,13 @@
+// creating a hello-world in node.js
+
+const http = require('http'); 
+const port = 3000
+
+debugger
+http.createServer((req,res) => {
+    res.writeHead('200',{'Content-Type': 'text/plain'})
+    debugger
+    res.end('Hello, World\n')
+}).listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
