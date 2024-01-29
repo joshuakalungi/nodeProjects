ec7f21ba1f35d77889d806e7ca9d476b82d6425a
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-29 21:56:58 +0300

    working with fs-sync module

---
+++ fs_sync.js
@@ -0,0 +1,9 @@
+// making use of synchronous methods in fs module
+const { readFileSync, writeFileSync } = require('fs');
+
+const first = readFileSync('./content/subfolder/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+
+console.log(`${first}, ${second}`);
+
+writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'});
