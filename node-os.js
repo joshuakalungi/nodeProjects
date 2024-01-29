b0330f363cc1438bd6d39dcd537c3d5d727360cc
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-29 20:05:03 +0300

    working with os module

---
+++ node-os.js
@@ -0,0 +1,11 @@
+// wprk with os module
+const os = require('os')
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+console.log(currentOS)
