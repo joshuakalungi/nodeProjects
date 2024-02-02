2e3b8e8abfcd28be35d6295ab4c4504be3e0c334
joshuakalungi <joshuakalungi101@gmail.com>
2024-02-02 14:35:39 +0300

    working with the request object

--- express-setup/app.js
+++ express-setup/app.js
@@ -6,13 +6,34 @@ const http = require('http')
 const server = http.createServer((req,res)=> {
     console.log("user hit the server")
 
-    // passing response data
-    res.writeHead(200, {"content-type":"text/html"})
+    // request url
+    const url = req.url;
 
-    // passing the data
-    res.write('<h1>homepage</h1>')
-    // signal that the communication is over
-    res.end()
+    if(url === '/'){
+        // passing response data
+        res.writeHead(200, {"content-type":"text/html"})
+
+        // passing the data
+        res.write('<h1>homepage</h1>')
+        // signal that the communication is over
+        res.end();
+    }else if(url === '/about'){
+        // passing response data
+        res.writeHead(200, {"content-type":"text/html"})
+
+        // passing the data
+        res.write('<h1>about page</h1>')
+        // signal that the communication is over
+        res.end()
+    }else{
+        // passing response data
+        res.writeHead(404, {"content-type":"text/html"})
+
+        // passing the data
+        res.write('<h1>page not found</h1>')
+        // signal that the communication is over
+        res.end()
+    }
 })
 
 server.listen(5000)
