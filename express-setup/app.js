5812754378da0f3a2a17465613171f0024c36ccd
joshuakalungi <joshuakalungi101@gmail.com>
2024-02-02 14:10:02 +0300

    created an http server

--- express-setup/app.js
+++ express-setup/app.js
@@ -1,1 +1,12 @@
 console.log("everything was a success")
+
+
+const http = require('http')
+
+const server = http.createServer((req,res)=> {
+    console.log("user hit the server")
+    // signal that the communication is over
+    res.end('home page')
+})
+
+server.listen(5000)
