af3d560fb9b2d290edbcd82fce9f1d3246847df8
joshuakalungi <joshuakalungi101@gmail.com>
2024-02-02 14:16:04 +0300

    specifying response data type

--- express-setup/app.js
+++ express-setup/app.js
@@ -5,8 +5,14 @@ const http = require('http')
 
 const server = http.createServer((req,res)=> {
     console.log("user hit the server")
+
+    // passing response data
+    res.writeHead(200, {"content-type":"text/html"})
+
+    // passing the data
+    res.write('<h1>homepage</h1>')
     // signal that the communication is over
-    res.end('home page')
+    res.end()
 })
 
 server.listen(5000)
